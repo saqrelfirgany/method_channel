9e9d144889c7b4b4e23381cd9af30052c82b6cf6
saqrelfirgany <ahmad.amer@namaait.com>
2023-05-18 16:01:07 +0300

    kotlin code

--- android/app/src/main/kotlin/com/saqrelfirgany/method_channel/MainActivity.kt
+++ android/app/src/main/kotlin/com/saqrelfirgany/method_channel/MainActivity.kt
@@ -1,6 +1,30 @@
 package com.saqrelfirgany.method_channel
 
+import android.os.Build.VERSION
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val batteryChannel = "saqrelfirgany/battery"
+
+    private lateinit var channel: MethodChannel
+
+    @Override
+    fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, batteryChannel)
+        channel.setMethodCallHandler { call, result ->
+            if (call.method == "getBatteryLevel") {
+                val arguments = call.arguments as Map<*, *>
+                val name = arguments["name"]
+                val batteryLevel = getBatteryLevel()
+                result.success(batteryLevel)
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel:Int
+        if (VERSION.SDK_INT >= VERSION.CODEN)
+    }
 }
---
+++ lib/home/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const batteryChannel = MethodChannel('saqrelfirgany/battery');
+  String batteryLevel = 'Waiting for battery level...';
+
+  Future getBatteryLevel() async {
+    final arguments = {'name': 'saqrelfirgany'};
+    dynamic newBatteryLevel = await batteryChannel.invokeListMethod('getBatteryLevel', arguments);
+    setState(() {
+      batteryLevel = '$newBatteryLevel';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Battery'),
+      ),
+      body: Center(child: Text(batteryLevel)),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsetsDirectional.all(16),
+        child: ElevatedButton(
+          onPressed: getBatteryLevel,
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(120, 50),
+          ),
+          child: const Text('Battery'),
+        ),
+      ),
+    );
+  }
+}
